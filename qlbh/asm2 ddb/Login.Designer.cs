416bd8cdc0674d943e83c6faa96a459be7daa574
nguyenvietchinh2505 <vietchinh25052004@gmail.com>
2024-12-09 23:30:08 +0700

    Add new file

---
+++ qlbh/asm2 ddb/Login.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace asm2_ddb
+{
+    partial class fLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnexit = new System.Windows.Forms.Button();
+            this.btgcreate = new System.Windows.Forms.Button();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.txtpassword = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnexit
+            // 
+            this.btnexit.Location = new System.Drawing.Point(500, 332);
+            this.btnexit.Margin = new System.Windows.Forms.Padding(4);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(100, 28);
+            this.btnexit.TabIndex = 13;
+            this.btnexit.Text = "Exit";
+            this.btnexit.UseVisualStyleBackColor = true;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // btgcreate
+            // 
+            this.btgcreate.Location = new System.Drawing.Point(377, 332);
+            this.btgcreate.Margin = new System.Windows.Forms.Padding(4);
+            this.btgcreate.Name = "btgcreate";
+            this.btgcreate.Size = new System.Drawing.Size(100, 28);
+            this.btgcreate.TabIndex = 12;
+            this.btgcreate.Text = "Create";
+            this.btgcreate.UseVisualStyleBackColor = true;
+            this.btgcreate.Click += new System.EventHandler(this.btgcreate_Click);
+            // 
+            // btnlogin
+            // 
+            this.btnlogin.Location = new System.Drawing.Point(253, 332);
+            this.btnlogin.Margin = new System.Windows.Forms.Padding(4);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(100, 28);
+            this.btnlogin.TabIndex = 11;
+            this.btnlogin.Text = "Login";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // txtpassword
+            // 
+            this.txtpassword.Location = new System.Drawing.Point(253, 289);
+            this.txtpassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 6);
+            this.txtpassword.Name = "txtpassword";
+            this.txtpassword.PasswordChar = '*';
+            this.txtpassword.Size = new System.Drawing.Size(345, 22);
+            this.txtpassword.TabIndex = 9;
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(255, 192);
+            this.txtusername.Margin = new System.Windows.Forms.Padding(4);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(345, 22);
+            this.txtusername.TabIndex = 7;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Label1.Location = new System.Drawing.Point(285, 79);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(177, 29);
+            this.Label1.TabIndex = 6;
+            this.Label1.Text = "DATstore Auto";
+            this.Label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(84, 185);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(148, 29);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "USERNAME";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(81, 282);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 29);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "PASSWORD";
+            this.label3.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // fLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImage = global::asm2_ddb.Properties.Resources.Screenshot_2024_12_05_194827;
+            this.ClientSize = new System.Drawing.Size(779, 452);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btgcreate);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.txtpassword);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.Label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fLogin";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button btnexit;
+        internal System.Windows.Forms.Button btgcreate;
+        internal System.Windows.Forms.Button btnlogin;
+        internal System.Windows.Forms.TextBox txtpassword;
+        internal System.Windows.Forms.TextBox txtusername;
+        internal System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
